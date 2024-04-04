b94a8232045fd18cc2e47c12302b567938c64b7b
sefakurtipek <skurtipek@gmail.com>
2024-04-04 23:15:10 +0300

    new codes added

---
+++ introduction/input_and_output.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int a, b, c;
+   cin >> a;
+   cin >> b;
+   cin >> c;
+   int sum = a + b + c;
+   std::cout << sum << std::endl;
+   return 0;
+}
